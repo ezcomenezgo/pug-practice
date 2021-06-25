f7791d4f9ed1629673418c62c00e3fe46cc5f4c5
Claire <s3310507057@gmail.com>
2021-06-25 11:07:09 +0800

    20210625

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 /*
  * @Author: your name
  * @Date: 2021-06-24 16:39:52
- * @LastEditTime: 2021-06-24 17:40:35
+ * @LastEditTime: 2021-06-25 10:00:31
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /pug-express/app.js
  */
 const express = require('express')
+const axios = require('axios')
 const app = express()
+
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', async (req, res) => {
+  const query = await axios.get('http://localhost:3001/results')
+  res.render('index', { employees: query.data })
 })
 
 app.listen(3000, () => {
